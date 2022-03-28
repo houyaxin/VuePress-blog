c4279a25d391b97b910ca7b452a5c8baffe65a23
houyaxin <724509494@qq.com>
2022-03-28 17:46:48 +0800

    pwa bug 解决

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,13 +22,11 @@ module.exports = {
       }
     ],
     [
-      plugins, {
-        '@vuepress/pwa': {
-          serviceWorker: true,
-          updatePopup: {
-            message: "发现新内容可用",
-            buttonText: "刷新"
-          }
+      '@vuepress/pwa',{
+        serviceWorker: true,
+        updatePopup: {
+          message: "发现新内容可用",
+          buttonText: "刷新"
         }
       }
     ]
